d3ef22dfc8d3530a8824e481eb789833b25a6b41
ephys <hello@guylian.me>
2021-06-26 13:17:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# graphql-non-null-directive
+
+Makes your GraphQL input optional, but not null!
+
+## The problem
+
+[GraphQL Thread from 2018](https://github.com/graphql/graphql-spec/issues/542)
+
+It's common for Update mutations to accept a partial input, in order to do a partial update.
+
+The simplest version of this is to simply omit the non-nullish (`!`) operator in the input declaration, and not update anything that's nullish:
+
+```graphql
+input UpdateUserInput {
+  firstName: String
+  lastName: String
+  country: String
+  # ...
+}
+```
+
+The issue with this is that you lose the ability to say "if provided, this field cannot be null"
+
+With a `@nonNull` directive, you get this ability back:
+
+```graphql
+input UpdateUserInput {
+  firstName: String @nonNull
+  lastName: String @nonNull
+  country: String
+  # ...
+}
+```
+
+## Usage
+
+**NB: This library is built using graphql-tools and should be compatible with apollo*
+
+- Install from npm: [`npm i @ephys/graphql-non-null-directive`](https://www.npmjs.com/package/@ephys/graphql-non-null-directive)
+- Declare the directive in your schema: 
+    ```graphql
+    directive @nonNull on INPUT_FIELD_DEFINITION
+    ```
+- Declare the directive in your Apollo Server:
+    ```typescript
+    import { createNonNullDirective } from '@ephys/graphql-non-null-directive';
+  
+    const server = new ApolloServer({
+      // ...
+      schemaDirectives: { 
+        nonNull: createNonNullDirective({
+          // (optional) use this property to change the ID of the directive
+          directiveName: 'nonNull',
+          // (optional) use this property to configure how Input Errors are built
+          //  Input Errors are thrown when a @nonNull input received null..
+          buildInputError: (message: string) => {
+            return new Error(message);
+          },
+        })
+      }
+    });
+    ```
---
+++ src/index.ts
@@ -0,0 +1,135 @@
+import { defaultFieldResolver, GraphQLInputObjectType, GraphQLNonNull, GraphQLObjectType } from 'graphql';
+import { SchemaDirectiveVisitor, SchemaDirectiveVisitorClass } from 'graphql-tools';
+import get from 'lodash/get';
+
+/*
+ * This @nonNull directive can be used on input fields that you want to make OPTIONAL but not NULLABLE
+ * Use it as a replacement for the required ("!") operator in such cases.
+ */
+
+type TCreateDirectiveOptions = {
+  directiveName?: string,
+  buildInputError?: (message: string) => any,
+};
+
+export function createNonNullDirective(opts?: TCreateDirectiveOptions): SchemaDirectiveVisitorClass {
+  const directiveName = opts?.directiveName ?? 'nonNull';
+  const buildInputError = opts?.buildInputError ?? ((message: string) => new Error(message));
+
+  const processedSchemas = new WeakSet();
+
+  class NonNullDirective extends SchemaDirectiveVisitor {
+    // TODO: Add support for ARGUMENT_DEFINITION (currently only INPUT_FIELD_DEFINITION is supported) (the arg var itself needs to be checked)
+    //  need to go through `visitArgumentDefinition` because their AST is not available in `visitInputFieldDefinition`
+    //
+    // visitArgumentDefinition(argument: GraphQLArgument, details: {
+    //   field: GraphQLField<any, any>;
+    //   objectType: GraphQLObjectType | GraphQLInterfaceType;
+    // }) {
+    //   console.log('visitArgumentDefinition');
+    //   this.checkField(argument);
+    //   this.processSchema();
+    // }
+    visitInputFieldDefinition(visitedInputField) {
+      this.checkField(visitedInputField);
+      this.processSchema();
+    }
+
+    processSchema() {
+      if (processedSchemas.has(this.schema)) {
+        return;
+      }
+
+      // @nonNull tags inputs (parameters)
+      // but the only way to enforce @nonNull is to transform the `resolve` function of the field that use the input.
+      // GraphQl.js doesn't give us the list of fields that use a given input,
+      // so we're iterating the whole schema to find them
+
+      const typeMap = this.schema.getTypeMap(); // checking `input {}` definition
+
+      function checkInputObject(inputType: GraphQLInputObjectType, taggedInputs, prefix = []) {
+        for (const field of Object.values(inputType.getFields())) {
+          // find nested objects
+          // unpack "!" operator
+          let fieldType = field.type;
+
+          if (fieldType instanceof GraphQLNonNull) {
+            fieldType = fieldType.ofType;
+          }
+
+          if (fieldType instanceof GraphQLInputObjectType) {
+            // sub-input objects
+            checkInputObject(fieldType, taggedInputs, [...prefix, field.name]);
+          } // find @nonNull directives
+          // this is the tagged input property!
+
+          const isNonNull = field.astNode.directives.find(d => d.name.value === directiveName);
+
+          if (isNonNull) {
+            taggedInputs.push([...prefix, field.name]);
+          }
+        }
+      }
+
+      for (const type of Object.values(typeMap)) {
+        if (!(type instanceof GraphQLObjectType)) {
+          continue;
+        }
+
+        for (const field of Object.values(type.getFields())) {
+          const taggedInputs = [];
+
+          for (const arg of field.args) {
+            let argType = arg.type; // Unpack the "!" operator
+            // NB. we're not unpacking lists because @nonNull does not make sense inside the list, use "!" for that.
+            //  so @nonNull only tags the list itself
+
+            if (argType instanceof GraphQLNonNull) {
+              argType = argType.ofType;
+            }
+
+            if (argType instanceof GraphQLInputObjectType) {
+              checkInputObject(argType, taggedInputs, [arg.name]);
+            }
+          }
+
+          if (taggedInputs.length > 0) {
+            taggedInputs.sort((a, b) => a.length - b.length);
+            const {
+              resolve = defaultFieldResolver,
+            } = field;
+
+            field.resolve = function resolveFn(...resolverArgs) {
+              const fieldArgs = resolverArgs[1];
+
+              for (const inputPath of taggedInputs) {
+                // only throw if === null, because then:
+                // if an object has an optional property, which itself has a required property,
+                //  don't throw if the optional property is missing, but do if the nested one is
+                if (get(fieldArgs, inputPath) === null) {
+                  throw buildInputError(`${inputPath.join('.')} cannot be null`);
+                }
+              }
+
+              return resolve.apply(this, resolverArgs);
+            };
+          }
+        }
+      }
+
+      processedSchemas.add(this.schema);
+    }
+
+    checkField(field) {
+      const fieldName = field.astNode.name.value;
+
+      if (field.type instanceof GraphQLNonNull) {
+        throw new Error(`@nonNull cannot be used on a field that is already non-nullish (! operator): "${fieldName}: ${field.type}"`);
+      }
+
+      return field;
+    }
+  }
+
+  return NonNullDirective;
+}
